8c9cd04ecf5fbabbdaa706694867359ae42b396c
Paul Boiseau <paulboiseau@gmail.com>
2015-11-12 15:16:03 +0100

    Exercice 2

--- code/implem/array_processor.rb
+++ code/implem/array_processor.rb
@@ -1,3 +1,29 @@
 class ArrayProcessor
 
+  attr_accessor(:my_array)
+
+  def initialize(array)
+    @my_array = array
+  end
+
+  def self.sort(array)
+    array.sort
+  end
+
+  def my_array_sorted
+    self.class.sort @my_array
+  end
+
+  def contains?(value)
+    @my_array.include? value
+  end
+
+  def self.double(array)
+    double_array = []
+    array.each do |value|
+      double_array.push(value.to_s + value.to_s)
+    end
+    return double_array
+  end
+
 end
--- code/implem/string_processor.rb
+++ code/implem/string_processor.rb
@@ -2,9 +2,10 @@ class StringProcessor
 
   def self.count(string, occurrence = nil)
     if occurrence.nil?
-      return string.size
+      string.size
+    else
+      string.downcase.count(occurrence.downcase)
     end
-    string.downcase.count(occurrence.downcase)
   end
 
   def self.concat(string1, string2)
