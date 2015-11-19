1a12cf5051c393d34c9044a2f2b67d47e54d3e1f
Paul Boiseau <paulboiseau@gmail.com>
2015-11-19 21:03:55 +0100

    post test ok

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,26 @@
 class Post < ActiveRecord::Base
   belongs_to :user
 
+  validates :user, presence: true
+
+  before_save { |post| post.title = post.title.capitalize unless post.title.nil? }
+
+  scope :published, -> { where(published: true) }
+
+  def length
+    return nil if self.message == ''
+    self.message.size
+  end
+
+  def unpublish!
+    self.published = false
+  end
+
+  def self.unpublish_all
+    self.all.each do |post|
+      post.unpublish!
+      post.save!
+    end
+  end
+
 end
